da55bb57224245ff51e145fb3bddecb767eae669
hungphu2151 <20110540@student.hcmute.edu.vn>
2023-08-17 14:06:17 +0700

    Tái cấu trúc, áp dụng PropTypes

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,5 +1,6 @@
+import PropTypes from 'prop-types';
 import className from 'classnames/bind';
-import Header from '~/components/Layouts/components/Header';
+import Header from '~/layouts/components/Header';
 import Sidebar from './Sidebar';
 import styles from './DefaultLayout.module.scss';
 
@@ -16,5 +17,9 @@ function DefaultLayout({ children }) {
         </div>
     );
 }
+
+DefaultLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 
 export default DefaultLayout;
