8b0f511ce0ed23ffc75fef4a469b6483c98d0325
hungphu2151 <20110540@student.hcmute.edu.vn>
2023-08-17 16:15:41 +0700

    Tối ưu header

--- src/layouts/DefaultLayout/Sidebar/index.js
+++ src/layouts/DefaultLayout/Sidebar/index.js
@@ -1,14 +1,1 @@
-import className from 'classnames/bind';
-import styles from './Sidebar.module.scss';
-
-const cx = className.bind(styles);
-
-function Sidebar() {
-    return (
-        <aside className={cx('wrapper')}>
-            <h2>SideBar</h2>
-        </aside>
-    );
-}
-
-export default Sidebar;
+export { default } from './Sidebar';
